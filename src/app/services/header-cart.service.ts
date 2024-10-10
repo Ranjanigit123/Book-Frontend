c2ee61b5c75f84cea66d124b0047c6e3ad5d179d
Ranjanigit123 <ranjanirithu206@gmail.com>
2024-10-10 19:45:41 +0530

    Initial commit

--- src/app/services/header-cart.service.ts
+++ src/app/services/header-cart.service.ts
@@ -4,21 +4,28 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Book } from '../models/book.model';
+import { environment } from '../../environments/environment'; // Adjust path based on your file structure
+
+//import { environment } from '../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
+
+
 export class HeaderCartService {
     private cart: Book[] = [];
     private cartSubject = new BehaviorSubject
     <Book[]>(this.cart);
     cart$ = this.cartSubject.asObservable();
+    private apiUrl = environment.baseURl;
 
     constructor(private http: HttpClient) { }
 
     addToCart(book: Book) {
         this.cart.push(book);
         this.cartSubject.next(this.cart);
+        return this.http.post(`${this.apiUrl}/cart`, { book });
     }
 
     removeFromCart(book: Book) {
@@ -27,6 +34,8 @@ export class HeaderCartService {
         if (index !== -1) {
             this.cart.splice(index, 1);
             this.cartSubject.next(this.cart);
+            return this.http.delete(`${this.apiUrl}/cart/${book._id}`);
         }
+        return undefined;
     }
 }
