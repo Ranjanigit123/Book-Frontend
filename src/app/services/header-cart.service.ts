bcb9198a4f45ef0dee3112fc916b035e0cc3170a
Ranjanigit123 <ranjanirithu206@gmail.com>
2024-10-11 17:56:21 +0530

    Initial commit

--- src/app/services/header-cart.service.ts
+++ src/app/services/header-cart.service.ts
@@ -1,31 +1,24 @@
 // services/header-cart.service.ts
 
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Book } from '../models/book.model';
-//import { environment } from '../../environments/environment'; // Adjust path based on your file structure
-
-//import { environment } from '../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
-
-
 export class HeaderCartService {
     private cart: Book[] = [];
     private cartSubject = new BehaviorSubject
     <Book[]>(this.cart);
     cart$ = this.cartSubject.asObservable();
-    //private apiUrl = environment.baseURl;
 
     constructor(private http: HttpClient) { }
 
     addToCart(book: Book) {
         this.cart.push(book);
         this.cartSubject.next(this.cart);
-       // return this.http.post(`${this.apiUrl}/cart`, { book });
     }
 
     removeFromCart(book: Book) {
@@ -34,11 +27,6 @@ export class HeaderCartService {
         if (index !== -1) {
             this.cart.splice(index, 1);
             this.cartSubject.next(this.cart);
-            //return this.http.delete(`${this.apiUrl}/cart/${book._id}`);
         }
-        //return undefined;
     }
-    //getBooks(): Observable<Book[]> {
-        //return this.http.get<Book[]>(`${this.apiUrl}/books`);
-    //}    
 }
